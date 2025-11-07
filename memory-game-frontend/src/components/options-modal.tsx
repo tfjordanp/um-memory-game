bb85e576c2bb4895982c1e8d49596fa3b6540a65
TJordanP <tjordanp004@gmail.com>
2025-11-07 02:34:16 +0100

    Fixed bug on options modal

--- memory-game-frontend/src/components/options-modal.tsx
+++ memory-game-frontend/src/components/options-modal.tsx
@@ -17,7 +17,7 @@ function OptionsModal({show,handleClose}:ReturnType<typeof useModalState>){
                 <Form.Check // prettier-ignore
                     type="switch"
                 >
-                    <Form.Check.Input style={{width: '5rem',height: '2.5rem'}} id='audio-switch' onChange={e => appModel?.setState(model => ({...model, audioEnabled: !model?.audioEnabled}))} />
+                    <Form.Check.Input checked={appModel?.state?.audioEnabled} style={{width: '5rem',height: '2.5rem'}} id='audio-switch' onChange={e => appModel?.setState(model => ({...model, audioEnabled: !model?.audioEnabled}))} />
                     <Form.Check.Label style={{fontSize: '2rem',paddingLeft: '5.5rem',paddingRight: '5.5rem'}} htmlFor='audio-switch'>{appModel?.state?.audioEnabled ? "Turn off audio" : "Turn on audio"}</Form.Check.Label>
                </Form.Check> 
             </Modal.Body>
